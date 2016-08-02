8ef9be0b797012e7f70cc467f1a716f2b080d922
Ravi Wu <raviwu@gmail.com>
2016-08-02 09:13:14 +0800

    add image to ebooks for cover image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801221321) do
+ActiveRecord::Schema.define(version: 20160801225929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 20160801221321) do
     t.index ["user_id"], name: "index_entries_on_user_id", using: :btree
   end
 
+  create_table "images", force: :cascade do |t|
+    t.string   "asset_file_name",    null: false
+    t.string   "asset_content_type", null: false
+    t.integer  "asset_file_size",    null: false
+    t.datetime "asset_updated_at",   null: false
+    t.integer  "entry_id",           null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["entry_id"], name: "index_images_on_entry_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email",           null: false
     t.string "username",        null: false
@@ -51,4 +62,5 @@ ActiveRecord::Schema.define(version: 20160801221321) do
 
   add_foreign_key "attachments", "entries"
   add_foreign_key "entries", "users"
+  add_foreign_key "images", "entries"
 end
